471e1e333c8134b3930c6a2cea6512ce5f4354a4
quyenld9699 <quyenld9699@gmail.com>
2021-08-21 09:29:54 +0700

    up project

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+io.on('connection', (socket) => {
+    console.log("usser connected")
+
+    socket.on('onchat', data => {
+        io.emit("user_chat", data)
+    })
+})
+
+server.listen(4000, () => {
+    console.log("listen to port 4000")
+})
